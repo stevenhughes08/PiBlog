c2a2c6c0610975b1764d6fb0435e0f54a543e4fd
Steven <stevenhughes08@gmail.com>
2020-01-01 13:28:29 -0500

    updates routes, added login.html and updated base.html

--- p1/app/routes.py
+++ p1/app/routes.py
@@ -1,6 +1,12 @@
 from app import app
 from flask import Flask, render_template
+from app.forms import LoginForm
 
+
+@app.route('/login')
+def loin():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
 @app.route('/')
 @app.route('/index')
 def index():

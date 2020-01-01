b20079b68bb8b7ba8314fcc28ead75ed40132dea
Steven <stevenhughes08@gmail.com>
2020-01-01 17:33:41 -0500

    updates routes.py to support dynamic linking

--- p1/app/routes.py
+++ p1/app/routes.py
@@ -1,16 +1,16 @@
 from app import app
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for
 from app.forms import LoginForm
 
 
 @app.route('/login', methods=['GET', 'POST'])
-def loin():
+def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login request for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data
         ))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 @app.route('/')
 @app.route('/index')

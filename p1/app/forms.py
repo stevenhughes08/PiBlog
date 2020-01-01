725597cecde496073373f8f598a112fffc0a53b1
Steven <stevenhughes08@gmail.com>
2020-01-01 13:40:20 -0500

    Updated problems with routes.py and linin.html

--- p1/app/forms.py
+++ p1/app/forms.py
@@ -4,6 +4,6 @@ from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    Password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
